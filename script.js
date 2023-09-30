399cdb0b5ac2670345542c19e6bde6425407a94c
jannkadc <jannkadc@gmail.com>
2023-09-30 14:41:06 +0800

    Edited the appearance of display messages

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const holes = [...document.querySelectorAll('.hole')];
 const mole = document.querySelector('.mole');
 const countdownEl = document.querySelector('#countdown');
 const scoreEl = document.querySelector('#score');
+const gameOverEl = document.getElementById("gameOver");
 const startBtn = document.querySelector('.startBtn')
 
 
@@ -11,19 +12,20 @@ let maxMissLimit = 5;
 let gameDuration = 15; //seconds
 let countdownInterval = null;
 let gameRunning = false;
-let hitPosition;
+let pokePosition;
 let missTimer = null;
 
 function startGame() {
     initializeGameTimer();
     gameRunning = true;
     startBtn.style.display = 'none';
-    countdownEl.style.display = 'block'
-    showFroggyInterval = setInterval(showFroggy, showFroggyInterval()) 
+    gameOverEl.style.display = 'none';
+    showFroggyInterval = setInterval(showFroggy, getShowInterval()) 
 }
 
 function initializeGameTimer() {
     countdownInterval = setInterval( ()=>{
+        scoreEl.style.display = 'block';
         countdownEl.style.display = 'block';
         countdownEl.innerHTML = `Time Left : ${gameDuration}`;
         gameDuration--;
@@ -34,7 +36,7 @@ function initializeGameTimer() {
     }, 1000)
 }
 
-function showFroggyInterval() {
+function getShowInterval() {
     if(score >= 0 && score <= 5) {
         return 1000;
     } else if(score >= 6 && score <= 10) {
@@ -44,38 +46,47 @@ function showFroggyInterval() {
     }
 }
 
+
 function showFroggy() {
     if(gameRunning) {
+
         if(miss === maxMissLimit){
             endGame();
             return;
         }
+
         const randomHole = holes[Math.floor(Math.random() * holes.length)];
         randomHole.classList.add('mole');
-        hitPosition = randomHole.id
+
+        pokePosition = randomHole.id
 
         setTimeout( ()=>{
             randomHole.classList.remove('mole')
-        }, 800);
-        missTimer = setTimeout(()=>{
-            miss++;
-        }, 800)
+        }, 500);
+
     }
 }
+
 
 function endGame() {
     clearInterval(countdownInterval);
     clearInterval(showFroggyInterval);
     countdownEl.style.display = 'none';
+    scoreEl.style.display = 'none'
+    gameOverEl.style.display = 'block';
+    gameOverEl.innerHTML = `Game Over. Your Score is ${score}`;
     score = 0;
     miss = 0;
     gameDuration = 15;
     startBtn.style.display = 'block'
 }
 
+
 holes.forEach(hole =>{
     hole.addEventListener('click', ()=>{
-        if(hole.id == hitPosition){
+        if(hole.id == pokePosition){
+            console.log(hole.id);
+            console.log(pokePosition)
             score++;
             scoreEl.innerHTML = `Score : ${score}`;
         } else {
@@ -83,6 +94,8 @@ holes.forEach(hole =>{
         }
     })
 })
+ 
+
 
 
 
